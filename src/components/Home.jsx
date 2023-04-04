f16d29becec047ae2826846a5e30304a07c3903a
adamprinciotta <adamprinciotta@gmail.com>
2023-04-04 19:01:11 -0400

    Added video. Would like to have the second video to cycle with it, but I'll try that next time

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,38 @@
-import '../styles/Home.css'
+import "../styles/Home.css";
+import React from "react";
+import video from "../images/kitchenVideo.mp4";
+// import video2 from "../images/kitchenVideo2.mp4";
+
+import { useState } from "react";
 
 function Home() {
+  const [isFirstVideo, setIsFirstVideo] = useState(true);
+
   return (
-    <div>Hello this is home</div>
-  )
+    <div className="homeContainer">
+      <section className="mainScren">
+        <div className="video-container">
+          {/* {isFirstVideo ? (
+            <video autoPlay muted onEnded={handleVideoEnded}>
+              <source src={video} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <video autoPlay muted onEnded={handleVideoEnded}>
+              <source src={video2} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          )}
+          <div className="content">Content test</div> */}
+          <video autoPlay muted loop>
+            <source src={video} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        Hello there!
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
