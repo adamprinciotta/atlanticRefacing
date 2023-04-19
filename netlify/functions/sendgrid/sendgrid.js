23e47dd31f0066af3878f2e0d91b3708ace46c7b
adamprinciotta <adamprinciotta@gmail.com>
2023-04-19 10:37:36 -0400

    Switched to axios

--- netlify/functions/sendgrid/sendgrid.js
+++ netlify/functions/sendgrid/sendgrid.js
@@ -1,4 +1,60 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+// import { sendEmail } from "@netlify/emails";
+
+// export async function handler(event, context) {
+//   if (event.body === null) {
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify("Payload required"),
+//     };
+//   }
+
+//   const formData = JSON.parse(event.body);
+
+
+//   try{
+//       const response = await fetch(`${process.env.URL}/.netlify/functions/emails/sendGridEmail`, {
+//       headers: {
+//         "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
+//       },
+//       method: "POST",
+//       body: JSON.stringify({
+//         from: "adamprinciotta@gmail.com",
+//         to: "adamprinciotta@gmail.com",
+//         subject: "New Estimate Request",
+//         parameters: {
+//           name: formData.name,
+//           city: formData.city,
+//           state: formData.state,
+//           homePhone: formData.homePhone,
+//           cellPhone: formData.cellPhone,
+//           email: formData.email,
+//         },
+//       }),
+//     });
+
+//     if(!response.ok){
+//       return{
+//         statusCode: response.status,
+//         body: JSON.stringify(`Email failed to send: ${await response.text()}`),
+//       };
+//     }
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify("Email sent!"),
+//     };
+//   }
+//   catch(error){
+//     return{
+//     statusCode: 500,
+//     body: JSON.stringify(`Server error: ${error}`)
+//     }
+//   }
+// };
+
+
+import axios from 'axios';
 import { sendEmail } from "@netlify/emails";
 
 export async function handler(event, context) {
@@ -11,32 +67,29 @@ export async function handler(event, context) {
 
   const formData = JSON.parse(event.body);
 
-
-  try{
-      const response = await fetch(`${process.env.URL}/.netlify/functions/emails/sendGridEmail`, {
+  try {
+    const response = await axios.post(`${process.env.URL}/.netlify/functions/emails/sendGridEmail`, {
+      from: "adamprinciotta@gmail.com",
+      to: "adamprinciotta@gmail.com",
+      subject: "New Estimate Request",
+      parameters: {
+        name: formData.name,
+        city: formData.city,
+        state: formData.state,
+        homePhone: formData.homePhone,
+        cellPhone: formData.cellPhone,
+        email: formData.email,
+      },
+    }, {
       headers: {
         "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
       },
-      method: "POST",
-      body: JSON.stringify({
-        from: "adamprinciotta@gmail.com",
-        to: "adamprinciotta@gmail.com",
-        subject: "New Estimate Request",
-        parameters: {
-          name: formData.name,
-          city: formData.city,
-          state: formData.state,
-          homePhone: formData.homePhone,
-          cellPhone: formData.cellPhone,
-          email: formData.email,
-        },
-      }),
     });
 
-    if(!response.ok){
-      return{
+    if (!response.status === 200) {
+      return {
         statusCode: response.status,
-        body: JSON.stringify(`Email failed to send: ${await response.text()}`),
+        body: JSON.stringify(`Email failed to send: ${response.statusText}`),
       };
     }
 
@@ -44,13 +97,10 @@ export async function handler(event, context) {
       statusCode: 200,
       body: JSON.stringify("Email sent!"),
     };
-  }
-  catch(error){
-    return{
-    statusCode: 500,
-    body: JSON.stringify(`Server error: ${error}`)
-    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(`Server error: ${error}`)
+    };
   }
 };
-
-
